32b0681541c0e4faf34edab3d975bb07aca746e1
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-03-30 20:02:05 -0600

    def js functions
    nota breve expliacion funciones js

--- javascript/index1.js
+++ javascript/index1.js
@@ -1,6 +1,25 @@
-4+3;
+/*4+3;
 
 if(4+3===7){
 	alert("Eres crack!");
 	console.log('Y un lobo .w.')
+}*/
+
+function saludar(){
+	console.log('Hola mundo cruel :v');
 }
+
+saludar();
+
+var despedir = function(){
+	console.log('Adios');
+}
+
+despedir();
+
+function multiplicar(a, b){
+	
+	return console.log(a*b);
+}
+
+multiplicar(5,10);
