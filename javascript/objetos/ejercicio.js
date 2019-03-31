c1da13e966aa9434ad929cd142473b22ebb49f86
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-03-31 01:55:16 -0600

    def objetos
    notas iniciales uso objetos

---
+++ javascript/objetos/ejercicio.js
@@ -0,0 +1,31 @@
+// Crea un objeto y una matriz que usaremos en nuestro ejercicio de Facebook.
+
+// 1. Crear un objeto que tenga propiedades "nombre de usuario" y "contraseña". Completa esos valores con cadenas.
+{
+	username: "andrei",
+	password: "supersecret"
+}
+
+// 2. Cree una matriz que contenga el objeto que ha creado anteriormente y nombre la "base de datos" array
+var database = [
+	{
+		username: "andrei",
+		password: "supersecret"
+	}
+];
+
+// 3. Cree una matriz llamada "newsfeed" que contiene 3 objetos con propiedades "username" y "timeline".
+var newsfeed = [
+	{
+		username: "Bobby",
+		timeline: "¡Tan cansado de todo ese aprendizaje!"
+	},
+	{
+		username: "Sally",
+		timeline: "Javascript es cool!"
+	},
+	{
+		username: "Mitch",
+		timeline: "Javascript es muy bueno!"
+	}
+];
