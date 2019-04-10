d394c0700a68d0979ec65738a3445d9470b396ca
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-04-10 02:36:51 -0500

    def proyecto final de curso
    avance componentes iniciales

--- detectarostro/src/App.js
+++ detectarostro/src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Particles from 'react-particles-js';
+import Navigation from './componentes/Navigation';
+import Logo from './componentes/Logo';
+import ImageLinkForm from './componentes/ImageLinkForm';
+import Rank from './componentes/Rank';
 import './App.css';
+
+const opcionesParticulas = {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <Particles className='particulas'
+      params={opcionesParticulas}
+      />      
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+        {/*<FaceRecognition /> */}
       </div>
     );
   }
---
+++ detectarostro/src/componentes/Logo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+import brain from './brain.png';
+import './Logo.css';
+
+const Logo = () => {
+	return (
+		<div className='ma4 mt0' >
+			<Tilt className="Tilt" options={{ max:55 }} style={{ height:100, width:100 }} >
+				<div className="Tilt-inner pa3">
+				<img style={{ paddingTop:'4px' }} alt='cerebro' src={brain} />
+				</div>
+			</Tilt>
+		</div>
+	);
+}
+
+export default Logo;
